125cac8b5058909dfb48251fc6153dcc8943e456
Jonas Dourado <jonas.jonaias@gmail.com>
2012-09-10 02:11:09 -0300

    Tex done

--- EPC3/ex1.hs
+++ EPC3/ex1.hs
@@ -19,6 +19,9 @@ activeSet u = u > 0
 alphaCut :: Float -> Float -> Bool
 alphaCut a u = u >= a
 
+complement :: Float -> Float
+complement = ((-)1)
+
 -- Ex 1.
 
 funcA :: Float -> Float
@@ -36,24 +39,35 @@ funcD  = triangleFunc 25 35 45
 funcE :: Float -> Float
 funcE  = trapezoidFunc 35 45 50 50
 
-discurseUniverse :: [Float]
-discurseUniverse = discretize 0 50 1000
+discurseUniverse :: Int -> [Float]
+discurseUniverse = discretize 0 50 
+
+discurseUniverse50 :: [Float]
+discurseUniverse50 = discurseUniverse 50
+
+discurseUniverse1000 :: [Float]
+discurseUniverse1000 = discurseUniverse 1000
+
+discurseUniverse500 :: [Float]
+discurseUniverse500 = discurseUniverse 500
+
+
 
 -- 1. b)
 
---map funcA discurseUniverse
---map funcB discurseUniverse
---map funcC discurseUniverse
---map funcD discurseUniverse
---map funcE discurseUniverse
+--map funcA discurseUniverse1000
+--map funcB discurseUniverse1000
+--map funcC discurseUniverse1000
+--map funcD discurseUniverse1000
+--map funcE discurseUniverse1000
 
 --1. c)
 
--- map (activeSet.funcA) discurseUniverse
--- map (activeSet.funcB) discurseUniverse
--- map (activeSet.funcC) discurseUniverse
--- map (activeSet.funcD) discurseUniverse
--- map (activeSet.funcE) discurseUniverse
+-- map (activeSet.funcA) discurseUniverse1000
+-- map (activeSet.funcB) discurseUniverse1000
+-- map (activeSet.funcC) discurseUniverse1000
+-- map (activeSet.funcD) discurseUniverse1000
+-- map (activeSet.funcE) discurseUniverse1000
 
 --1. d)
 
@@ -65,8 +79,36 @@ discurseUniverse = discretize 0 50 1000
 
 --1. e)
 
--- map ((alphaCut X).funcA) discurseUniverse
--- map ((alphaCut X).funcB) discurseUniverse
--- map ((alphaCut X).funcC) discurseUniverse
--- map ((alphaCut X).funcD) discurseUniverse
--- map ((alphaCut X).funcE) discurseUniverse
+-- map ((alphaCut X).funcA) discurseUniverse1000
+-- map ((alphaCut X).funcB) discurseUniverse1000
+-- map ((alphaCut X).funcC) discurseUniverse1000
+-- map ((alphaCut X).funcD) discurseUniverse1000
+-- map ((alphaCut X).funcE) discurseUniverse1000
+
+-- Ex2 
+
+uA = map funcA discurseUniverse1000
+uB = map funcB discurseUniverse1000
+uC = map funcC discurseUniverse1000
+uD = map funcD discurseUniverse1000
+uE = map funcE discurseUniverse1000
+
+op :: (Float -> Float -> Float) -> [Float]
+op o = zipWith o (zipWith o (zipWith o (zipWith o uA uB) uC ) uD) uE
+
+--2. b)
+
+ex2b = op max
+
+--2. c)
+
+ex2c = op min
+
+--2. d)
+
+ex2d = map complement uC
+
+--3. a)
+
+
+-- main = print (map ((alphaCut 0.5).funcA) discurseUniverse)
