0ead6cc9ad4120c440ba0d2acd634b24571f05fd
Jonas Dourado <jonas.jonaias@gmail.com>
2012-10-29 00:20:20 -0200

    Added max average to determine fuzzy system output

--- EPC7/epc7.py
+++ EPC7/epc7.py
@@ -226,9 +226,23 @@ if __name__ == "__main__":
             output_function_d = table[(active_dht,active_vf)]
             after_mamdani = numpy.minimum(using_conective_e,output_function_d)
             #plot_graph(x_classe,after_mamdani)
-            #agregação
+            #agregacao
             valor_final = numpy.maximum(valor_final, after_mamdani)
-            print "Final value:", valor_final
+    
+    #~ print "Final value:", valor_final
+    max_values_position = map(lambda u: False if u <> max(valor_final) else True, valor_final)
+    print max_values_position
+    
+    total_sum=0
+    
+    for i in range(len(max_values_position)):
+        if max_values_position[i]:
+            total_sum+=i
+            
+    class_value = total_sum/numpy.sum(max_values_position)
+
+    print class_value
+    
             #~ print vf_value, "->", active_vf, "->", active_vf(vf_value)
             #~ print dht_value, "->", active_dht, "->", active_dht(dht_value)
                      
